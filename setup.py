eb5a0ecca56a2e9deaa9143aa74155d1d2b519de
Mathias Hedström <mathias.hedstrom.mh@gmail.com>
2015-08-24 15:10:12 +0200

    added setup.py, tox.ini and empty test files

---
+++ setup.py
@@ -0,0 +1,17 @@
+# pylint: disable=missing-docstring
+from setuptools import setup
+
+setup(
+    name='software-statement',
+    version='',
+    packages=['software_statement'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    install_requires=[
+        "oic",
+    ],
+    author='DIRG',
+    author_email='',
+    description=''
+)
